67e89502bc40fe5bc6de6ea9394235dfd7d0f92a
Oksana Letner <135754081+LetnerOk@users.noreply.github.com>
2023-08-31 13:01:07 +0300

    Update last_paid_click.sql
    Add datamart for the Last Paid Click attribution model with ROW_NUMBER

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -22,3 +22,28 @@ SELECT *
 FROM Last_Paid_Click
 ORDER BY visit_date, utm_source, utm_medium, utm_campaign
 ;
+
+--Datamart for the Last Paid Click attribution model with ROW_NUMBER
+WITH Last_Paid_Click AS
+(
+SELECT 
+    s.visitor_id,
+    visit_date,
+    source AS utm_source,
+    medium AS utm_medium,
+    campaign AS utm_campaign,
+    lead_id,
+    created_at,
+    amount,
+    closing_reason,
+    status_id,
+    ROW_NUMBER() OVER(PARTITION BY s.visitor_id ORDER BY visit_date DESC) rn
+FROM sessions s
+LEFT JOIN leads l
+ON s.visitor_id = l.visitor_id 
+WHERE medium <> 'organic' 
+)
+SELECT *
+FROM Last_Paid_Click
+WHERE rn = 1
+ORDER BY visit_date, utm_source, utm_medium, utm_campaign
